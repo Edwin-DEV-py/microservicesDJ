af142c05f13ed17e254b5ba70eac7dcca08d36d8
edwin-papasito <nicolas.tarazona.morales@gmail.com>
2023-07-01 02:23:38 -0500

    urls

--- adminsite/products/views.py
+++ adminsite/products/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
+from rest_framework.response import Response
 from .serializers import ProductSerializer
 from rest_framework import viewsets
+from .models import Product
 
 class ProductViewSet(viewsets.ViewSet):
     
     def list(self, request): #/api/products
-        pass
+        products = Product.objects.all()
+        serializer = ProductSerializer(products,many=True)
+        return Response(serializer.data)
         
     def create(self,request): #/api/products
         pass
