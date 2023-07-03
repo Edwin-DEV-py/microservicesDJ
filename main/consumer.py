3b441e2ae91a8e8d22c7ba7adae3d2c5e0079ffc
edwin-papasito <nicolas.tarazona.morales@gmail.com>
2023-07-02 23:51:17 -0500

    microservicio main

--- main/consumer.py
+++ main/consumer.py
@@ -1,4 +1,10 @@
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
+django.setup()
 import pika, json
+from products.models import Product
 
 params = pika.URLParameters('amqps://rlfcgigm:snIyM-j0ycanTqVvNYhQfNPN2mZ9CK9G@jackal.rmq.cloudamqp.com/rlfcgigm')
 
@@ -15,11 +21,29 @@ def callback(ch, method,properties, body):
     data = json.loads(body)
     print(data)
     
+
     if properties.content_type == 'producto_creado':
-        print('producto creado')
+        print(data)
+        product, created = Product.objects.get_or_create(id=data['id'], defaults={'title': data['title'], 'image': data['image']})
+        print('producto creado =>', created)
+
+
         
     elif properties.content_type == 'producto':
         print('lista')
+        
+    elif properties.content_type == 'producto_editado':
+        product = Product.objects.get(id=data['id'])
+        product.title = data['title']
+        product.image = data['image']
+        product.save()
+        print('Producto actualizado =>', product)
+
+    
+    elif properties.content_type == 'producto_eliminado':
+        product = Product.objects.get(id=data)
+        product.delete()
+        print('producto eliminado=')
 
 
 channel.basic_consume(queue='main', on_message_callback=callback, auto_ack=True)
--- main/products/models.py
+++ main/products/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 class Product(models.Model):
-    id = models.IntegerField(primary_key=True)
     title = models.CharField(max_length=200)
     image = models.CharField(max_length=200)
     
